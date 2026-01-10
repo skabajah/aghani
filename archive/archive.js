2ff9d665014eeaec88608b3a7abe871201fc070d
S. Kabajah <shadi.kabajah@gmail.com>
2026-01-10 16:51:30 -0500

    br

--- archive/archive.js
+++ archive/archive.js
@@ -58,7 +58,7 @@
     ].filter(Boolean).join(" | ");
 
     section.innerHTML = `
-      <h3>${esc(item.title)}</h3>
+      <br><h3>${esc(item.title)}</h3>
       ${item.banner ? `<img class="cover" src="${esc(item.banner)}" >` : ""}
       ${links ? `<h5 class="archive-links">${links}</h5>` : ""}
 
